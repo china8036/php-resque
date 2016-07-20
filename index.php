6d656cb2e9dcba113f35c60508a24c4375915591
wzh <wangzhenhong@chaoma.me>
2016-07-20 14:25:32 +0800

    样列逻辑微调

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ define('BASE_ROOT', __DIR__);
 define('DS', DIRECTORY_SEPARATOR);
 include_once BASE_ROOT . DS . 'src' . DS . 'Core.php';
 use core\Core as Core;
+use core\Work as Work;
 Core::load();
-$work = new \core\Work(Core::c('redis_backend'), Core::c('redis_backend_db'));
+$work = new Work(Core::c('redis_backend'), Core::c('redis_backend_db'), Core::c('prefix'));
 $work->queue(Core::c('queue'));
 
--- jobs/Crontab.php
+++ jobs/Crontab.php
@@ -3,7 +3,6 @@
 /**
  * @copyright (c) 2916, Ryan [CHAOMA.ME]
  */
-use Resque;
 use core\Log;
 
 class Crontab implements core\intface\Job
@@ -11,9 +10,7 @@ class Crontab implements core\intface\Job
 
     public function perform()
     {
-        sleep(1);
-        $jobId = Resque::enqueue('crontab', 'Crontab', ['time' => microtime(true)], true); //循环投递用来模拟定时器 测一下延时
-        Log::record('crontab', [$jobId, $this->args]);
+        Log::record('crontab_jobs', [microtime(true),$this->args]);
     }
 
 }
