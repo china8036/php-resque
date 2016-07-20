60b072f202d62e9c3727af7a4366b14b05961639
wzh <wangzhenhong@chaoma.me>
2016-07-20 14:24:45 +0800

    配置信息更改

--- config/config.php
+++ config/config.php
@@ -6,6 +6,7 @@
 return [
     'redis_backend' => '127.0.0.1:6379',
     'redis_backend_db' => null,//redis 数据库名称
+    'prefix' => null,//前缀 多个php-resque时候用于区分
     'queue' => [//监控的任务队列， 和监控的进程数 多个用','分开 '*'为监控所有队列
         ['high',1],
         ['high,normal',2],
