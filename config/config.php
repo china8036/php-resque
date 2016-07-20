f59e4d324ab97c3092419db62de77588cee9cbc2
wzh <wangzhenhong@chaoma.me>
2016-07-20 14:20:13 +0800

    提交配置文件

--- config/config.php
+++ config/config.php
@@ -7,9 +7,11 @@ return [
     'redis_backend' => '127.0.0.1:6379',
     'redis_backend_db' => null,//redis 数据库名称
     'queue' => [//监控的任务队列， 和监控的进程数 多个用','分开 '*'为监控所有队列
-        ['crontab', 1],//定时任务占用
         ['high',1],
         ['high,normal',2],
         ['high,normal,low', 3],
+    ],
+    'crontab' => [
+        '1' => ['Crontab'],
     ]
 ];
